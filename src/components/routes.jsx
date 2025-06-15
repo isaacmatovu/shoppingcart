4d0d4a577b9c8599a45ed072941769fc01bb4f8d
isaacmatovu <matovuisaac646@gmail.com>
2025-06-15 11:56:02 +0300

    added shop route

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,60 +1,40 @@
-
-
-import { HomePage,Shop,Cart } from "./lazyComponents";
+import { HomePage, Shop, Cart } from "./lazyComponents";
 import { routePaths } from "./routes.config";
 import LazyWrapper from "./lazyWrapper";
 import Layout from "./layout";
 
+const routes = [
+  {
+    path: routePaths.home,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: (
+          <LazyWrapper>
+            <HomePage />
+          </LazyWrapper>
+        ),
+      },
 
-
-
-
-
-
-
-const routes=[
-
-
-    {
-        path: routePaths.home,
-        element:<Layout/>,
-        children: [
-
-            {
-               index: true,
-               element: (
-                <LazyWrapper>
-                    <HomePage/>
-                </LazyWrapper>
-               )
-
-            },
-
-            {
-                path:"shop",
-                element: (
-                    <LazyWrapper>
-                        <Shop/>
-                    </LazyWrapper>
-                )
-            },
-            {
-                path:"cart",
-                element:  (
-                    <LazyWrapper>
-                        <Cart/>
-                    </LazyWrapper>
-                )
-            },
-           
-          
-        ],
-        
-       
-    }
-    
-
-    
-]
+      {
+        path: "shop",
+        element: (
+          <LazyWrapper>
+            <Shop />
+          </LazyWrapper>
+        ),
+      },
+      {
+        path: "cart",
+        element: (
+          <LazyWrapper>
+            <Cart />
+          </LazyWrapper>
+        ),
+      },
+    ],
+  },
+];
 
 export default routes;
